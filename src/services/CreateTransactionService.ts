d311ec988c0b86350cbe9fb4ec59814d014d9eb3
Moisés Escurra <moisesmagno@gmail.com>
2020-12-23 16:00:00 -0300

    Criado o registrao de transações.

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface RequestCreate {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,18 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: RequestCreate): Transaction {
+    if (type !== 'income' && type !== 'outcome') {
+      throw Error('The operation type is wrong!');
+    }
+
+    const transation = this.transactionsRepository.create({
+      title,
+      value,
+      type
+    });
+
+    return transation;
   }
 }
 
