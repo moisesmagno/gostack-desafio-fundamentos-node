34ddea5b3e60d1b5218b6e6a88df3d96afa50a0c
Moisés Escurra <moisesmagno@gmail.com>
2020-12-24 21:18:44 -0300

    Criado as últimas funcionalidades que são referentes ao retorno dos income, outcome e balance.

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -21,11 +21,37 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    return this.transactions;
+    const transactions = this.transactions;
+  
+    return transactions;
+  
   }
 
-  public getBalance(): Balance {
-    // TODO
+   public getBalance(): Balance {
+    
+    const balance = this.transactions.reduce((accumulator: Balance, transaction: Transaction) => {
+    switch(transaction.type){
+        case 'income':
+          accumulator.income += parseInt(transaction.value);
+          break;
+        case 'outcome':
+          accumulator.outcome += parseInt(transaction.value);
+          break;
+        default: 
+          break;
+    }
+
+      accumulator.total = parseInt(accumulator.income) - parseInt(accumulator.outcome);
+      
+      return accumulator;       
+    
+    }, {
+      income: 0,
+      outcome: 0,
+      total: 0
+    });
+    
+    return balance;    
   }
 
   public create({title, value, type}: CreateTransaction): Transaction {
